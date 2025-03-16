c030fbf33637c1b2763f3c935875cf8628d6a230
Nathaniel Martes <nathaniel.martes@wilkes.edu>
2025-03-16 18:29:19 -0400

    Added basic tokenizing from HTML page

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module wilkesu-scrapy/src
 
-go 1.18
+go 1.23.0
+
+toolchain go1.23.7
 
 require go.mongodb.org/mongo-driver v1.17.3
 
@@ -12,7 +14,8 @@ require (
 	github.com/xdg-go/scram v1.1.2 // indirect
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20240726163527-a2c0da244d78 // indirect
-	golang.org/x/crypto v0.26.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
-	golang.org/x/text v0.17.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/net v0.37.0
+	golang.org/x/sync v0.12.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
 )
--- src/scraper.go
+++ src/scraper.go
@@ -2,9 +2,62 @@ package main
 
 import (
 	"fmt"
-	// "net/http"
+	"net/http"
+	"io/ioutil"
+	"strings"
+	"golang.org/x/net/html"
 )
 
+
+func getHTML(link string) string {
+	/* getHTML gets the HTML from a webpage.
+
+	Arguments:
+		link (string): The link to get the HTML from.
+	
+	Returns:
+		string: The HTML from the webpage.
+	*/
+
+	resp, err := http.Get(link)
+	defer resp.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(body)
+}
+
+func parseHTML(body string) {
+	/* parseHTML looks at a string of HTML, and tokenizes it using
+	Golang's html tokenizer.
+
+	Arguments:
+		body (string): The body of html to parse.
+	
+	Returns:
+		Umm . . . Cheddar!
+	*/
+	tokenizer := html.NewTokenizer(strings.NewReader(body))
+	for {
+		tokenType := tokenizer.Next()
+		if tokenType == html.ErrorToken {
+			// EOF: Done reading
+			return
+		}
+		token := tokenizer.Token()
+		fmt.Println(token.String())
+	}
+}
 func scraper() {
 	fmt.Println("Scraper service started")
+
+	body := getHTML("https://rosters.wilkes.edu/scheds/coursesF25.html")
+
+	parseHTML(body)
 }
