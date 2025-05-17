b8f1248265db358817e36ab08fc67d09fcb6ba4a
Nathaniel Martes <nathaniel.martes@wilkes.edu>
2025-05-17 12:29:27 -0400

    Added Card component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,33 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Card from "./Card/Card.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Card/>
+      <Card 
+        header="F2F - PHA 425 (A) Kieck D"
+        title="Pharmacotherapeutics III"
+        credits="3.00"
+        time="M 2 - 3:50PM SLC B05; TRF 2 - 3:50PM SLC 105"
+        crn="31146"
+        students="55"
+        limit="105"
+      />
     </>
   )
 }
---
+++ client/src/Card/Card.jsx
@@ -0,0 +1,31 @@
+import "./Card.css";
+import copy from "../assets/copy-link-icon.svg"
+
+function Card({header = "Header", title = "Title", extra_info = "Extra Info", 
+              time = "MWF 9 - 9:50AM SLC 101", crn = "00000", 
+              credits = 3.00, students = 0, limit = 1}) {
+  return (
+    <>
+      <div className="main">
+        <p className="header">{header}</p>
+        <p className="title">{title}</p>
+        <p className="credits">Credits: {parseFloat(credits).toFixed(2)}</p>
+        <p className="extra_info">{extra_info}</p>
+        <p className="time">{time}</p>
+        <div className="bottom_bar">
+          <div className="crn">
+            <p>CRN: {crn}</p>
+            <img src={copy} alt="Copy" width="24" height="24"/>
+          </div>
+          <div className="padding"></div>
+          <div className="students">
+            <p> {students} / {limit}</p>
+          </div>
+          <button className="card_button">{students / limit == 1 ? "Closed": "Add"}</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Card;
